8e7b38f943a90dcbb03032ab15f6cef2cc34cb37
Sébastien Randriamanana <sebastienrandriamanana3@gmail.com>
2024-08-25 23:04:53 +0300

    add news

--- src/app/news.service.ts
+++ src/app/news.service.ts
@@ -7,12 +7,12 @@ import { Observable } from 'rxjs';
 })
 export class NewsService {
   private apiKey = '2a45cfd87b3a4fefb32d7d651fe1477c';
-  private apiUrl = 'https://newsapi.org/v2/everything';
+  private apiUrl = 'v2/everything';
 
   constructor(private httpClient : HttpClient) { }
 
   getNews():Observable<any>{
     const query ='weather OR climate OR storm OR rain OR temperature';
-    return this.httpClient.get<any>(`${this.apiUrl}?q=${query}&pageSize=10&apikey=${this.apiKey}`)
+    return this.httpClient.get<any>(`/api/${this.apiUrl}?q=${query}&pageSize=10&apikey=${this.apiKey}`)
   }
 }
